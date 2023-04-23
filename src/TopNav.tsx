d84ae6105f74e146a0a905e3e0b050fb6a8b0c82
Travis Nielsen <trniel@microsoft.com>
2023-04-23 14:51:55 -0500

    navbar updates

--- src/TopNav.tsx
+++ src/TopNav.tsx
@@ -1,4 +1,6 @@
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -10,11 +12,10 @@ const TopNav = () => {
       <Container>
         <Navbar.Brand href="#home">Azure Diagrammer</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown title="Filter" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -26,6 +27,25 @@ const TopNav = () => {
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
+
+          <Navbar.Collapse className="justify-content-end">
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as: <a href="#login">Demo User</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
