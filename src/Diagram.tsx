e2177f3790e02bd1b3c25a3688cb496400ed7b65
Travis Nielsen <trniel@microsoft.com>
2023-05-02 00:00:47 -0500

    filter on edge click. layout optimizations

--- src/Diagram.tsx
+++ src/Diagram.tsx
@@ -1,70 +1,97 @@
-import { Canvas, NodeProps, CanvasRef, NodeData, EdgeData, EdgeProps } from 'reaflow';
+import { Canvas, NodeProps, CanvasRef, NodeData, EdgeData, EdgeProps, ElkRoot } from 'reaflow';
 import PrepareNode from './nodes'
 import PrepareEdge from './edges'
 import { getNodeData, getEdgeData } from './data'
-import { useCallback, useRef, useState, useEffect, MutableRefObject, FC } from 'react';
+import { useRef, useState } from 'react';
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import './App.css'
 
+let selectedNodeId = '';
 
 const Diagram: React.FC = () => {
 
-  const canvasRef: MutableRefObject<CanvasRef | null> = useRef<CanvasRef | null>(null);
-
+  const canvasRef = useRef<CanvasRef>(null);
   const [nodeData, edgeData] = loadCanvasData();
   const [nodes, setNodes] = useState(nodeData);
   const [edges, setEdges] = useState(edgeData);
+  const [paneWidth, setPaneWidth] = useState(1000);
+  const [paneHeight, setPaneHeight] = useState(1000);
 
-  function handleNodeUpdate (nodes: NodeData[], edges: EdgeData[]) {
+  function handleNodeUpdate(nodes: NodeData[], edges: EdgeData[], nodeId: string) {
+    selectedNodeId = nodeId;
     setNodes(nodes);
     setEdges(edges);
+    centerOnSelectedNode(nodeId);
   }
 
+  // TOTO: This needs research.  It's not working as expected.
+  function centerOnSelectedNode(nodeId: string) {
+
+    const canvasWidth = canvasRef?.current?.canvasWidth;
+    const canvasHeight = canvasRef?.current?.canvasHeight;
+
+    if (canvasWidth && canvasHeight) {
+
+      const canvasItem = canvasRef.current?.layout.children?.find((item) => item.id === nodeId);
+      const { x, y, width, height } = canvasItem;
+      const canvasCenterX = canvasWidth / 2;
+      const canvasCenterY = canvasHeight / 2;
+      const canvasItemCenterX = x + (width / 2);
+      const canvasItemCenterY = y + (height / 2);
+      // const offsetX = canvasCenterX - canvasItemCenterX;
+      // const offsetY = canvasCenterY - canvasItemCenterY;
+
+      if (canvasRef?.current?.setScrollXY) {
+        canvasRef?.current?.setScrollXY([x, y]);
+      }
+
+    }
+  }
+  
   // TODO: Look at this for canvas re-sizing: https://github.com/reaviz/reaflow/issues/111
   // TODO: Also see: https://github.com/reaviz/reaflow/issues/190
   // TODO: https://github.com/reaviz/reaflow/issues/190
   // TODO: For centering on node: https://github.com/reaviz/reaflow/issues/64
-  const [paneWidth, setPaneWidth] = useState(1000);
-  const [paneHeight, setPaneHeight] = useState(1000);
+  function handleLayoutChange(layout: ElkRoot) {
 
-  const calculatePaneWidthAndHeight = useCallback(() => {
-      let newHeight = 0;
-      let newWidth = 0;
-      canvasRef?.current?.layout?.children?.forEach((node) => {
-        if (node.y + node.height > newHeight) newHeight = node.y + node.height;
-        if (node.x + node.width > newWidth) newWidth = node.x + node.width;
-      });
-      setPaneHeight(newHeight);
-      setPaneWidth(newWidth);
-  }, []);
-  
-  useEffect(() => {
-    canvasRef?.current?.fitCanvas?.();
-  }, [canvasRef])
+    let newHeight = 0;
+    let newWidth = 0;
+    layout.children?.forEach((node) => {
+      if (node.y + node.height > newHeight) newHeight = node.y + node.height;
+      if (node.x + node.width > newWidth) newWidth = node.x + node.width;
+    });
 
+    // keep a minimum pane size
+    if (newHeight < 1000) newHeight = 2000;
+    if (newWidth < 1000) newWidth = 2000;
+
+    setPaneHeight(newHeight / 2);
+    setPaneWidth(newWidth / 1.5);
+  }
 
   return (
-    <div>
-      <TransformWrapper wheel={{ step: 0.2 }} minScale={0.2} maxScale={8} limitToBounds={false} >
-        <TransformComponent wrapperStyle={{ backgroundColor: 'black' }} >
-            <Canvas
-              ref={canvasRef}
-              maxHeight={8000}
-              maxWidth={3000}
-              layoutOptions={{
-                'elk.hierarchyHandling': 'INCLUDE_CHILDREN',        // required to enable edges from/to nested nodes
-                'elk.nodeLabels.placement': 'INSIDE V_TOP H_RIGHT'
-              }}
-              direction='RIGHT'
-              nodes={nodes}
-              edges={edges}
-              fit={true}
-              node={(node: NodeProps) => PrepareNode(node, nodes, edges, handleNodeUpdate )}
-              edge={(edge: EdgeProps) => PrepareEdge(edge)}
-              onLayoutChange={() => {
-                calculatePaneWidthAndHeight()
-              }}
-            />
+    <div className='canvas-container'>
+      <TransformWrapper wheel={{ step: 0.2 }} minScale={0.2} maxScale={8} limitToBounds={false} centerOnInit={true} >
+        <TransformComponent wrapperStyle={{ backgroundColor: 'black', margin: 'auto' }} wrapperClass={'canvas-wrapper'} >
+          <Canvas
+            className='canvas-test'
+            ref={canvasRef}            
+            maxHeight={paneHeight}
+            maxWidth={paneWidth}
+            // height={paneHeight}
+            // width={paneWidth}
+            layoutOptions={{
+              'elk.hierarchyHandling': 'INCLUDE_CHILDREN',        // required to enable edges from/to nested nodes
+              'elk.nodeLabels.placement': 'INSIDE V_TOP H_RIGHT'
+            }}
+            direction='RIGHT'
+            nodes={nodes}
+            edges={edges}
+            fit={true}
+            node={(node: NodeProps) => PrepareNode(node, nodes, edges, handleNodeUpdate)}
+            edge={(edge: EdgeProps) => PrepareEdge(edge, nodes, edges, handleNodeUpdate)}
+            onLayoutChange={(layout) => { handleLayoutChange(layout) }}
+          />
         </TransformComponent>
       </TransformWrapper>
     </div>
--- src/edges.tsx
+++ src/edges.tsx
@@ -1,29 +1,95 @@
-import { Console } from 'console';
-import { write } from 'fs';
-import { Edge, EdgeData, EdgeProps } from 'reaflow';
+import { Node, NodeData, NodeProps, Edge, EdgeData, EdgeProps } from 'reaflow';
 
-const PrepareEdge = (edge: any) => {
+const PrepareEdge = (edge: EdgeProps, nodeData: NodeData[], edgeData: EdgeData[], handleNodeUpdate: Function ) => {
 
-    const edgeProps = edge.properties
+    const getParentNodes: any = (node: NodeData, nodeData: NodeData[]) => {
+        const parentNodes = nodeData.filter(parentNode => {
+            if (parentNode.id === node.parent) {
+                return true;
+            }
+            return false;
+        });
 
-    if (edge.data === undefined) {
+        if (parentNodes.length === 0) {
+            return [];
+        }
+
+        return [...parentNodes, ...getParentNodes(parentNodes[0], nodeData)];
+    }
+
+
+    // returns updated nodeData and edgeData
+    const filterNodesByEdge = () => {
+
+        let selectedNodeId = '';
+        
+        const connectedNodes = nodeData.filter(node => {
+            if (node.id === edge.target) {
+                selectedNodeId = node.id;
+                return true;
+            }
+            if (node.id === edge.source) {
+                return true;
+            }
+
+            return false;
+        });
+
+        const connectedNodesWithParents = connectedNodes.map(node => {
+            const parentNodes = getParentNodes(node, nodeData);
+            return [...parentNodes, node];
+        }).flat();
+
+        // return new edgedata that contains only the edge
+        const filteredEdgeData = edgeData.filter(edgeData => {
+            if (edgeData.id === edge.id) {
+                return true;
+            }
+            return false;
+        });
+
+        // get child nodes from filteredNodeData
+        const childNodes = connectedNodes.map(node => {
+            const children = nodeData.filter(childNode => {
+                if (childNode.parent === node.id) {
+                    return true;
+                }
+                return false;
+            });
+            return children;
+        }).flat();
+
+        return { nodes: [...childNodes, ...connectedNodesWithParents], edges: filteredEdgeData, selectedNodeId: selectedNodeId };
+    }
+
+    const onEdgeClick = (event: React.MouseEvent<SVGGElement, MouseEvent>) => {
+        console.log(`edge clicked (${edge.id})`);
+        const results = filterNodesByEdge();
+        handleNodeUpdate(results.nodes, results.edges, results.selectedNodeId);
+    };
+    
+    const onEdgeEnter = (event: React.MouseEvent<SVGGElement, MouseEvent>) => {
+        console.log(`edge entered (${edge.id})`);
+      };
+
+    if (edge.properties?.data === undefined) {
         return (
-            <Edge {...edgeProps} />
+            <Edge {...edge} onClick={onEdgeClick} />
         )
     }
 
-    switch (edge.data.type) {
+    switch (edge.properties?.data.type) {
         case 'vnetintegration':
             return (
-                <Edge {...edgeProps} style={{ stroke: 'blue' }} className='edge' />
+                <Edge {...edge} style={{ stroke: 'blue' }} className='edge' onClick={onEdgeClick} />
             )
         case 'vnet-peering':
             return (
-                <Edge {...edgeProps} style={{ stroke: 'purple' }} className='edge' />
+                <Edge {...edge} style={{ stroke: 'purple' }} className='edge' onClick={onEdgeClick} />
             )         
         default:
             return (
-                <Edge {...edgeProps} />
+                <Edge {...edge} onClick={onEdgeClick} />
             )
     }
 
